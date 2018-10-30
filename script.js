e6dd7d90a20b3d7fec8cbad01fb4c52c055f1e48
Susanne Pettersson <susannepet7427@gmail.com>
2018-10-30 11:33:14 +0100

    ändrat bakgrundsfärger

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 
 // ANCHORS
+var myBody = document.querySelector('body');
+var myHtml = document.querySelector('html');
 var allLinks = document.getElementsByClassName('links');
 var allatags = document.getElementsByClassName('a-tag');
 var home = document.getElementById('home');
@@ -16,7 +18,7 @@ var contact = document.querySelector('#contact');
 var home1 = document.querySelector('.home1');
 var aboutme2 = document.querySelector('.about-me2');
 var skills3 = document.querySelector('.skills3');
-var projects4 = document.querySelector('.projects4');
+var projects4 = document.querySelector('.project4');
 var post5 = document.querySelector('.post5');
 
 //fields
@@ -56,35 +58,86 @@ contact.addEventListener('click', doMore);
 // CALLBACKS
     
 // we use event object and switch statement	
-function doStuff(ev) {			
+/*function doStuff(ev) {			
     // we get the 'id' of the clicked link
     var targetId = ev.target.id;
    console.log (targetId);
     switch(targetId) {					
         case ('home'):
+        
         resetField(homeField.id);						
             home1.classList.toggle('home-hidden');
             break;
         case ('about-me'):
+        aboutField.style.backgroundColor = "transparent";
+        myBody.style.backgroundColor = "pink";
         resetField(aboutField.id);					
-            aboutme2.classList.toggle('about-me-hidden');
+        aboutme2.classList.toggle('about-me-hidden');
             break;
         case ('skills'):	
+        skillsField.style.backgroundColor = "transparent";
+        myBody.style.backgroundColor = "light-green";
         resetField(skillsField.id);					
-            skills3.classList.toggle('skills-hidden');
+        skills3.classList.toggle('skills-hidden');
             break;
             case ('projects'):	
+            projectField.style.backgroundColor = "transparent";
+            myBody.style.backgroundColor = "light-blue";
             resetField(projectField.id);		
             project4.classList.toggle('projects-hidden');
             break;
             case ('contact'):	
+            postField.style.backgroundColor = "transparent";
+            myBody.style.backgroundColor = "lilac";
             resetField(postField.id);					
             post5.classList.toggle('contact-hidden');
             break;
         default:
             break;
 
-    } }
+    } 
+}*/
+// we use event object and switch statement    
+function doStuff(ev) {
+    // we get the ‘id’ of the clicked link
+    var targetId = ev.target.id;
+
+    switch (targetId) {
+        case ('home'):
+        home.style.backgroundColor ="transparent";
+        myBody.style.backgroundColor ="2, 201,201";
+            resetField(homeField.id);            
+            home1.classList.toggle('home-hidden');
+            break;
+        case ('about-me'):
+            resetField(aboutField.id);
+            aboutField.style.backgroundColor = "transparent";
+            myBody.style.backgroundColor = "pink";
+            aboutme2.classList.toggle('about-me-hidden');
+            break;
+        case ('skills'):
+            resetField(skillsField.id);
+            skillsField.style.backgroundColor = "transparent";
+            myBody.style.backgroundColor = "yellow";
+            skills3.classList.toggle('skills-hidden');
+            break;
+        case ('projects'):
+            resetField(projectField.id);
+            projectField.style.backgroundColor = "transparent";
+            myBody.style.backgroundColor = "light-blue";
+            project4.classList.toggle('projects-hidden');
+            break;
+        case ('contact'):
+            resetField(postField.id);
+            postField.style.backgroundColor = "transparent";
+            myBody.style.backgroundColor = "gray";
+            post5.classList.toggle('contact-hidden');
+            break;
+        default:
+            break;
+    }
+}
+
     
     jQuery(document).ready(function(){
         jQuery('.skillbar').each(function(){
